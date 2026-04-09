0f6a41df07884ba7191fb32a37cdb1507259fdc1
TarunPatidar04 <tarun.coderfarm@gmail.com>
2026-04-09 23:45:52 +0530

    refactor: implement TryCatch middleware to streamline error handling in auth controller

--- auth/src/controllers/auth.ts
+++ auth/src/controllers/auth.ts
@@ -1,35 +1,28 @@
-import { Request, Response } from "express";
 import User from "../model/User.js";
 import jwt from "jsonwebtoken";
-export const loginUser = async (req: Request, res: Response) => {
-  try {
-    const { email, name, picture } = req.body;
+import TryCatch from "../middlewares/trycatch.js";
 
-    let user = await User.findOne({ email });
+export const loginUser = TryCatch(async (req, res) => {
+  const { email, name, picture } = req.body;
 
-    if (!user) {
-      user = await User.create({
-        email,
-        name,
-        image: picture,
-      });
-    }
+  let user = await User.findOne({ email });
 
-    const token = jwt.sign({ user }, process.env.JWT_SECRET_KEY as string, {
-      expiresIn: "15d",
-    });
-
-    return res.status(200).json({
-      message: "User logged in successfully",
-      success: true,
-      token,
-      user,
-    });
-  } catch (error:any) {
-    res.status(500).json({
-      message: error.message,
-      success: false,
-      error,
+  if (!user) {
+    user = await User.create({
+      email,
+      name,
+      image: picture,
     });
   }
-};
+
+  const token = jwt.sign({ user }, process.env.JWT_SECRET_KEY as string, {
+    expiresIn: "15d",
+  });
+
+  return res.status(200).json({
+    message: "User logged in successfully",
+    token,
+    user,
+    success: true,
+  });
+});
