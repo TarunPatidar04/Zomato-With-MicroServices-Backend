ba88fdef0a529f4fe4f95e638e6b9b23608a43ea
TarunPatidar04 <tarun.coderfarm@gmail.com>
2026-04-09 23:15:40 +0530

    create a user model and connect to mongodb database

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,13 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
+import connectDB from "./config/db.js";
 dotenv.config();
 
-
 const app = express();
-
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Auth Service is running on http://localhost:${PORT}`);
+  connectDB();
 });
