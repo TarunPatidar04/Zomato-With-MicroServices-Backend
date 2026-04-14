2e566b933f162b6620a0d80950d4665569bf9964
TarunPatidar04 <tarun.coderfarm@gmail.com>
2026-04-14 14:32:06 +0530

    fix: update Google OAuth redirect URI to postmessage and configure CORS for frontend origin

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,14 +2,19 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import authRoute from "./routes/auth.js";
+import cors from "cors";
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  }),
+);
 
 app.use("/api/auth", authRoute);
-
 
 const PORT = process.env.PORT || 5000;
 
